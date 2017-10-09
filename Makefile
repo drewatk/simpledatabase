ac0e6d2e30418872df45666a5f3c966d30e5f29e
Drew Atkinson <drewatkinson5@gmail.com>
2017-10-09 11:52:47 -0400

    file persistance

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 db: db.c
-	gcc db.c -o db
+	gcc -g db.c -o db
 
 run: db
 	./db mydb.db
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,11 @@
 describe 'database' do
+  before do
+    `rm -rf test.db`
+  end
+
   def run_script(commands)
     raw_output = nil
-    IO.popen("./db", "r+") do |pipe|
+    IO.popen("./db test.db", "r+") do |pipe|
       commands.each do |command|
         pipe.puts command
       end
@@ -76,6 +80,24 @@ describe 'database' do
     result = run_script(script)
     expect(result).to eq([
       "ID must be positive.",
+      "Executed."
+    ])
+  end
+
+  it 'keeps data after closing connection' do
+    result1 = run_script([
+      "insert 1 user1 person1@example.com",
+      ".exit",
+    ])
+    expect(result1).to eq([
+      "Executed."
+    ])
+    result2 = run_script([
+      "select",
+      ".exit",
+    ])
+    expect(result2).to eq([
+      "(1, user1, person1@example.com)",
       "Executed."
     ])
   end
